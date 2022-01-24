48f842e576cce46a65bf733a6517572fc23be3de
cggos <cggos@outlook.com>
2022-01-25 02:14:58 +0800

    move update_K() and update_P() from EKF to KF

--- include/estimator/kf.hpp
+++ include/estimator/kf.hpp
@@ -15,22 +15,21 @@ class KF : public StateEstimator, public Predictor {
 
   // virtual void update();
 
-  // template <class H_type, class R_type, class K_type>
-  // void update_K(const Eigen::MatrixBase<H_type> &H, const Eigen::MatrixBase<R_type> &R, Eigen::MatrixBase<K_type> &K)
-  // {
-  //   const auto &P = state_ptr_->cov;
-  //   const R_type &S = H * P * H.transpose() + R;
-  //   K = P * H.transpose() * S.inverse();
-  //   // const Eigen::Matrix<double, kStateDim, R_type::RowsAtCompileTime> K = P * H.transpose() * S.inverse();
-  // }
+  template <class H_type, class R_type, class K_type>
+  void update_K(const Eigen::MatrixBase<H_type> &H, const Eigen::MatrixBase<R_type> &R, Eigen::MatrixBase<K_type> &K) {
+    const auto &P = state_ptr_->cov;
+    const R_type &S = H * P * H.transpose() + R;
+    K = P * H.transpose() * S.inverse();
+    // const Eigen::Matrix<double, kStateDim, R_type::RowsAtCompileTime> K = P * H.transpose() * S.inverse();
+  }
 
-  // template <class H_type, class R_type, class K_type>
-  // void update_P(const Eigen::MatrixBase<H_type> &H,
-  //               const Eigen::MatrixBase<R_type> &R,
-  //               const Eigen::MatrixBase<K_type> &K) {
-  //   const MatrixSD &I_KH = MatrixSD::Identity() - K * H;
-  //   state_ptr_->cov = I_KH * state_ptr_->cov * I_KH.transpose() + K * R * K.transpose();
-  // }
+  template <class H_type, class R_type, class K_type>
+  void update_P(const Eigen::MatrixBase<H_type> &H,
+                const Eigen::MatrixBase<R_type> &R,
+                const Eigen::MatrixBase<K_type> &K) {
+    const MatrixSD &I_KH = MatrixSD::Identity() - K * H;
+    state_ptr_->cov = I_KH * state_ptr_->cov * I_KH.transpose() + K * R * K.transpose();
+  }
 
   virtual ~KF() {}
 };
