2b085c6fab2c2785851d5e73ffdf3db8ff1f0c05
cggos <cggos@outlook.com>
2022-01-25 23:32:18 +0800

    move update_K() and update_P() from KF to EKF

--- include/estimator/ekf.hpp
+++ include/estimator/ekf.hpp
@@ -32,6 +32,22 @@ class EKF : public KF {
     imu_model_.propagate_state_cov(last_imu, curr_imu, last_state, *state_ptr_);
   }
 
+  template <class H_type, class R_type, class K_type>
+  void update_K(const Eigen::MatrixBase<H_type> &H, const Eigen::MatrixBase<R_type> &R, Eigen::MatrixBase<K_type> &K) {
+    const auto &P = state_ptr_->cov;
+    const R_type &S = H * P * H.transpose() + R;
+    K = P * H.transpose() * S.inverse();
+    // const Eigen::Matrix<double, kStateDim, R_type::RowsAtCompileTime> K = P * H.transpose() * S.inverse();
+  }
+
+  template <class H_type, class R_type, class K_type>
+  void update_P(const Eigen::MatrixBase<H_type> &H,
+                const Eigen::MatrixBase<R_type> &R,
+                const Eigen::MatrixBase<K_type> &K) {
+    const MatrixSD &I_KH = MatrixSD::Identity() - K * H;
+    state_ptr_->cov = I_KH * state_ptr_->cov * I_KH.transpose() + K * R * K.transpose();
+  }
+
   ~EKF() {}
 };
 
